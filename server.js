1604c349a2fe12e37c2c41fe97b429ab2299a138
root <root@vultr.guest>
2019-01-13 06:20:56 +0000

    fix user get their own message marked as others

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const requestIp = require('request-ip');
 const fs = require('fs');
-const cors = require('cors');
+const cors = require('cors'); 
 
 const app = express();
-app.use(requestIp.mw()) /*解码用户IP*/
+app.use(requestIp.mw())	/*解码用户IP*/
 app.use(bodyParser.urlencoded({extended:false}));  /*解码传统urlForm*/
 app.use(bodyParser.json());/*解码JSON数据*/
-app.use(cors());        /*用于跨域名request*/
+app.use(cors());	/*用于跨域名request*/
 
 let chatText = [];
 let textID = 0;
@@ -16,48 +16,53 @@ let textID = 0;
 
 
 app.post('/getText',(req,res)=>{
-        
-        let reqArray = req.body.data.localText;
-        let reqTextID = reqArray.map(each=>each.textID);
-        let sendTextTemp = chatText.filter(each=>!reqTextID.includes(each.textID))
-        let sendText = sendTextTemp.map(each=>{
-                return {
-                                textID:each.textID,
-                                text:each.text,
-                                address:'remote',
-                                ipAddress:each.ipAddress,
-                                datetime:each.datetime
-                }
-        })
-        res.send(sendText);
+	let reqIp = req.clientIp.substring(7, );
+	
+	let reqArray = req.body.data.localText;
+	let reqTextID = reqArray.map(each=>each.textID);
+	let sendTextTemp = chatText.filter(each=>!reqTextID.includes(each.textID));
+	sendTextTemp = sendTextTemp.filter(each=>each.ipAddress!==reqIp);
+	
+	let sendText = sendTextTemp.map(each=>{
+		return {
+				textID:each.textID,
+				text:each.text,
+				address:'remote',
+				ipAddress:each.ipAddress,
+				datetime:each.datetime			
+		}
+	})
+	res.send(sendText);		
 
 })
 
 app.post('/chatText',(req,res)=>{
-        if(chatText.length>20){
-                chatText.shift();
-        }
+	if(chatText.length>25){
+		chatText.shift();
+	}
 
-        let text = req.body.chatText.text;
-        let datetime = req.body.chatText.datetime;
-        textID++;
-        chatText.push({
-                textID:textID,
-                text:text,
-                ipAddress:req.clientIp.substring(7, ),
-                datetime:datetime
-        })
-        let sendText = {
-                textID:textID,
-                text:text,
-                address:'local',
-                ipAddress: req.clientIp.substring(7, ),
-                datetime:datetime
-        }
+	let text = req.body.chatText.text;
+	let datetime = req.body.chatText.datetime;
+	textID++;
+	chatText.push({
+		textID:textID,
+		text:text,
+		ipAddress:req.clientIp.substring(7, ),
+		datetime:datetime
+	})
+	let sendText = {
+		textID:textID,
+		text:text,
+		address:'local',
+		ipAddress: req.clientIp.substring(7, ),
+		datetime:datetime
+	}
 
-        res.send(JSON.stringify(sendText));
-
+	res.send(JSON.stringify(sendText));
+	
 })
 
 app.listen(5000);
+
+
 
